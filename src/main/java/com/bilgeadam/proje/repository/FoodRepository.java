c2d12bc9db4c94b659c5a66e3062dfabd217286b
ruhhhh <63228770+ruhhhh@users.noreply.github.com>
2023-03-12 12:21:35 +0300

    Added food repository and drived query for food objects

--- src/main/java/com/bilgeadam/proje/repository/FoodRepository.java
+++ src/main/java/com/bilgeadam/proje/repository/FoodRepository.java
@@ -10,11 +10,35 @@ import java.util.UUID;
 @Repository
 public interface FoodRepository extends BaseRepository<Food, UUID> {
 
+    /**
+     * Example of derived query in spring data jpa
+     *
+     * @param title of food which has comments
+     * @return  Food object related with selected title
+     */
     Food findByIgnoreCaseTitle(String title);
 
+    /**
+     * Example of derived query in spring data jpa
+     *
+     * @param category of food which has category
+     * @return  list of Food objects related with selected category
+     */
     List<Food> findByCategory(String category);
 
+    /**
+     * Example of derived query in spring data jpa
+     *
+     * @param  price of food which has less than price
+     * @return  list of Food objects related with selected price
+     */
     List<Food> findByPriceLessThan(int price);
 
+    /**
+     * Example of derived query in spring data jpa
+     *
+     * @param  price of food which has greater than price
+     * @return  list of Food objects related with selected price
+     */
     List<Food> findByPriceGreaterThan(int price);
 }

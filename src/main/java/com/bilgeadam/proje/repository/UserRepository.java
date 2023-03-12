e492c3cc5309276678ad7a693d9d180e6eeed1ac
ahmettavus <ahmettavus40@gmail.com>
2023-03-12 12:11:58 +0100

    Version 0.0.7

--- src/main/java/com/bilgeadam/proje/repository/UserRepository.java
+++ src/main/java/com/bilgeadam/proje/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package com.bilgeadam.proje.repository;
 
 import com.bilgeadam.proje.common.repository.BaseRepository;
+import com.bilgeadam.proje.model.Role;
 import com.bilgeadam.proje.model.User;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.UUID;
@@ -11,13 +13,18 @@ public interface UserRepository extends BaseRepository<User, UUID> {
     List<User> findByUserNameContainingIgnoreCase(String username);
 
     //int deger varsa user icinde between ile arama
-    List<User> findbyAgeBetween(int ageStart,int ageEnd);
+    List<User> findByAgeBetween(int ageStart, int ageEnd);
 
     //email parametreli distinc ile arama
 
 
     //icinde or & and in oldugu bi query
 
-    //JPquel ile Driven query yaz
+    //JPQL ile Driven query yaz
+    //Role tablosunda name i olan ve userleri getiren, role ile user arasindaki iliskiyi getiren JPQL
+
+    @Query("SELECT c FROM users c INNER JOIN roles r ON Id WHERE r.name =?1")
+    List<Role> findByRoleName(String name);
+
 
 }

965564fdb1de921606a7d75ec07cd104dcf47420
hcemalpay <hcemalpay@gmail.com>
2023-03-25 19:42:12 +0300

    "version-10" Bütün contoller sınfları eklendi.

---
+++ src/main/java/com/bilgeadam/proje/controller/CustomerController.java
@@ -0,0 +1,122 @@
+package com.bilgeadam.proje.controller;
+
+import com.bilgeadam.proje.dto.CommentDto;
+import com.bilgeadam.proje.dto.FoodDto;
+import com.bilgeadam.proje.dto.UserDto;
+import com.bilgeadam.proje.service.CommentService;
+import com.bilgeadam.proje.service.FoodService;
+import com.bilgeadam.proje.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+import java.security.Principal;
+import java.util.UUID;
+
+@Controller
+//@RequestMapping("/customer")
+public class CustomerController
+{
+    private final UserService userService;
+    private final FoodService foodService;
+    private final CommentService commentService;
+
+    public CustomerController(UserService userService,
+                              FoodService foodService,
+                              CommentService commentService) {
+        this.userService = userService;
+        this.foodService = foodService;
+        this.commentService = commentService;
+    }
+
+    @RequestMapping(value = "/profile", method = {RequestMethod.GET, RequestMethod.POST})
+    public String showProfilePage(Principal principal, Model model) {
+
+        UserDto user = userService.findByUsername(principal.getName());
+        model.addAttribute("user", user);
+
+        return "customer/Profile";
+    }
+
+    @RequestMapping(value = "/profile-modify", method = {RequestMethod.GET, RequestMethod.POST})
+    public String showProfileModifyPage(Principal principal, Model model) {
+
+        UserDto user = userService.findByUsername(principal.getName());
+        user.setPassword("");
+        model.addAttribute("user", user);
+
+        return "customer/ProfileModify";
+    }
+
+    @RequestMapping("/profile-modify-action")
+    public String profileModifyAction(@Valid @ModelAttribute("user") UserDto updatedUser,
+                                      BindingResult bindingResult,
+                                      Principal principal) {
+
+        if (bindingResult.hasFieldErrors("email") || bindingResult.hasFieldErrors("password")
+                || bindingResult.hasFieldErrors("phoneNumber")) {
+            return "redirect:/profile-modify?error";
+        }
+
+        UserDto userDto = userService.findByUsername(principal.getName());
+        userDto.setEmail(updatedUser.getEmail());
+        userDto.setPassword(updatedUser.getPassword());
+        userDto.setPhoneNumber(updatedUser.getPhoneNumber());
+        userService.saveUser(userDto);
+
+        return "redirect:/profile?updated";
+    }
+
+    @RequestMapping(value = "/payment", method = {RequestMethod.GET, RequestMethod.POST})
+    public String showPaymentPage(@RequestParam("foodId") UUID foodId,
+                                  Model model,
+                                  HttpSession session) {
+
+        model.addAttribute("food", foodService.findById(foodId));
+        session.setAttribute("price", foodService.findById(foodId).getPrice());
+
+        return "customer/Payment";
+    }
+
+    @GetMapping("/getUserById/{id}")
+    UserDto getUserById(@PathVariable UUID id) {
+
+        return userService.findById(id);
+    }
+
+
+    @RequestMapping(value = "/comments/{foodId}", method = {RequestMethod.GET, RequestMethod.POST})
+    public String selectFoodID(@PathVariable("foodId") UUID foodId,
+                               HttpSession session) {
+
+        session.setAttribute("foodId", foodId);
+        return "redirect:/comments";
+    }
+
+    @RequestMapping(value = "/comments", method = {RequestMethod.GET, RequestMethod.POST})
+    public String showCommentPage(Model model, HttpSession session) {
+
+        UUID foodId = session.getAttribute("foodId") == null ? UUID.fromString("") : (UUID) session.getAttribute("foodId");
+        model.addAttribute("food", foodService.findById(foodId));
+
+        return "customer/Comments";
+    }
+
+    @RequestMapping(value = "/post-comments")
+    public String postComments(@RequestParam("foodId") UUID foodId,
+                               @RequestParam("comment") String postedComment,
+                               Principal principal) {
+
+        CommentDto commentDto = new CommentDto();
+        commentDto.setComment(postedComment);
+        commentDto.setPostedBy(principal.getName());
+        commentDto.setFoodId(foodId);
+
+        commentService.saveComment(commentDto);
+
+        return "redirect:/comments?posted";
+    }
+}
---
+++ src/main/java/com/bilgeadam/proje/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.bilgeadam.proje.repository;
+
+import com.bilgeadam.proje.common.repository.BaseRepository;
+import com.bilgeadam.proje.entity.Role;
+
+import java.util.UUID;
+
+public interface RoleRepository extends BaseRepository<Role, UUID> {
+
+
+}
